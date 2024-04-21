104e91d0a3d770885e87a3a70852ed88e1707e85
Luis De La Torre A <dev.delatorre.l@gmail.com>
2024-04-20 21:12:09 -0500

    Add project files.

---
+++ ParsingGame/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace ParsingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FilePath = @"E:\c#_projects\ParsingGame\ParsingGame\input.txt";
+            string OutputFile = @"E:\c#_projects\ParsingGame\ParsingGame\output.txt";
+            string[] SplitLine;
+
+            String[] splitLines = File.ReadAllLines(FilePath);
+            foreach (string line in splitLines)
+            {
+                if (line.Contains("split"))
+                {
+                    SplitLine = line.Split();
+
+                    using (StreamWriter writer = new StreamWriter(OutputFile, true))
+                    {
+                        writer.Write($"{SplitLine[4]} ");
+                    }
+                }
+            }
+        }
+
+    }
+}
